5cb14688e63618978d216f4bc5c732f05bafbaa0
kozlojak <jakub3008@gmail.com>
2019-02-07 16:49:22 +0100

    Add new System.out.println

--- src/main/java/pl/akubkozlowski/learning/MainClass.java
+++ src/main/java/pl/akubkozlowski/learning/MainClass.java
@@ -3,6 +3,7 @@ package pl.akubkozlowski.learning;
 public class MainClass {
 
   public static void main(String[] args) {
-
+    System.out.println("HEJ!");
+    System.out.println("Jestem Test1");
   }
 }
